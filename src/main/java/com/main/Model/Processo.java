e070b20039b9905b16c67610fecc672b14341272
Caique <asapcaique7@gmail.com>
2025-03-26 11:19:38 -0300

    classe de processo finalizada

--- src/main/java/com/main/Model/Processo.java
+++ src/main/java/com/main/Model/Processo.java
@@ -1,15 +1,63 @@
 package com.main.Model;
 
+import java.util.Random;
 
 public class Processo {
-    int prioridade;
-    TipoProcesso tipoProcesso;
+    private int id;
+    private int prioridade;
+    private int framesLimit;
+    private TempoUCP tempoUCP;
+
+    private TipoProcesso tipoProcesso;
+    
+    private static final Random random = new Random();
+
+    public Processo() {
+        this.id = generateRandomId();
+    }
+
+    private int generateRandomId() {
+        // Gera um ID aleatório no intervalo de 1000000 a 9999999
+        return random.nextInt(9000000) + 1000000; // 9000000 + 1000000 -> 1000000 até 9999999
+    }
+
+    public void setPrioridade(int prioridade){
+        if (prioridade < 1 || prioridade > 15) {
+            throw new IllegalArgumentException();
+        }
+        this.prioridade = prioridade;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public int getFramesLimit(){
+        return framesLimit;
+    }
+
+    public void setFramesLimit(int framesLimit) {
+        if (framesLimit < 1 || framesLimit > 5){
+            throw new IllegalArgumentException("Precisa ser de 1 a 5");
+        }
+        this.framesLimit = framesLimit;
+    }
+
+    public TipoProcesso getTipoProcesso() {
+        return tipoProcesso;
+    }
+
+    public void setTipoProcesso(TipoProcesso tipoProcesso) {
+        this.tipoProcesso = tipoProcesso;
+    }
 
     public enum TipoProcesso{
         CpuBound,
         IoBound
     }
 
-
-
+    public enum TempoUCP {
+        Pronto,
+        Execução
+    }
 }
